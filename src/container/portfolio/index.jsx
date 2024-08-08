b047127d96453ad9201f44fe4b0c17dbe0698ca9
vitthal-07 <vitthalbiradar50@gmail.com>
2024-08-08 12:18:02 +0530

    Fixed bugs

--- src/container/portfolio/index.jsx
+++ src/container/portfolio/index.jsx
@@ -2,59 +2,9 @@ import React, { useState } from "react";
 import "./style.scss";
 import { PageHeaderContent } from "../../components/pageHeaderComponent";
 import { BsFillBagCheckFill } from "react-icons/bs";
-import Youtube from "../../Images/Youtube_Clone.jpeg";
-import Amazon from "../../Images/Amazon_UI_Clone.avif";
-import Social from "../../Images/Social_Media.jpg";
-import Netflix from "../../Images/Netflix_UI_Clone.jpeg";
-import FoodApp from "../../Images/FoodOrderingApp.png";
 import { FaGitAlt } from "react-icons/fa";
 import { FaLink } from "react-icons/fa";
-
-const portfolioData = [
-    {
-        id: "2",
-        label: "Food Ordering App",
-        image: FoodApp,
-        repoLink: "https://github.com/vitthal-07/Food-Ordering-App-Frontend",
-        demoLink: "https://food-ordering-app-frontend-oz3l.onrender.com",
-    },
-
-    {
-        id: "2",
-        label: "Youtube Clone",
-        image: Youtube,
-        repoLink: "https://github.com/vitthal-07/Youtube-Clone",
-        demoLink: "https://youtube-clone-by-vitthal.netlify.app/",
-    },
-
-    {
-        id: "2",
-        label: "SocialSphere",
-        image: Social,
-        repoLink: "https://github.com/vitthal-07/SocialSphere-Using-React",
-        demoLink: "",
-    },
-    {
-        id: "2",
-        label: "Food Ordering App Backend",
-        image: FoodApp,
-        repoLink: "https://github.com/vitthal-07/Food-Ordering-App-Backend",
-    },
-    {
-        id: "2",
-        label: "Amazone UI Clone",
-        image: Amazon,
-        repoLink: "https://github.com/vitthal-07/Amazon-Clone",
-        demoLink: "",
-    },
-    {
-        id: "2",
-        label: "Netflix UI Clone",
-        image: Netflix,
-        repoLink: "https://github.com/vitthal-07/Netflix-Clone",
-        demoLink: "",
-    },
-];
+import { portfolioData } from "../../utils/portfolio-data";
 
 const filterData = [
     {
@@ -63,12 +13,12 @@ const filterData = [
     },
     {
         filterId: "2",
-        label: "Development",
+        label: "Frontend",
     },
-    // {
-    //     filterId: "3",
-    //     label: "Design",
-    // },
+    {
+        filterId: "3",
+        label: "Backend",
+    },
 ];
 
 export const Portfolio = () => {
---
+++ src/utils/portfolio-data.js
@@ -0,0 +1,59 @@
+import Youtube from "../Images/Youtube_Clone.jpeg";
+import Amazon from "../Images/Amazon_UI_Clone.avif";
+import Social from "../Images/Social_Media.jpg";
+import Netflix from "../Images/Netflix_UI_Clone.jpeg";
+import FoodApp from "../Images/FoodOrderingApp.png";
+import FoodApi from "../Images/FoodOrderingApi.png";
+import SocialMediaApi from "../Images/Social_Media_Api.png";
+
+export const portfolioData = [
+    {
+        id: "2",
+        label: "Food Ordering App",
+        image: FoodApp,
+        repoLink: "https://github.com/vitthal-07/Food-Ordering-App-Frontend",
+        demoLink: "https://food-ordering-app-frontend-oz3l.onrender.com",
+    },
+
+    {
+        id: "2",
+        label: "Youtube Clone",
+        image: Youtube,
+        repoLink: "https://github.com/vitthal-07/Youtube-Clone",
+        demoLink: "https://youtube-clone-by-vitthal.netlify.app/",
+    },
+
+    {
+        id: "2",
+        label: "SocialSphere",
+        image: Social,
+        repoLink: "https://github.com/vitthal-07/SocialSphere-Using-React",
+        demoLink: "",
+    },
+    {
+        id: "3",
+        label: "Food Ordering Api",
+        image: FoodApi,
+        repoLink: "https://github.com/vitthal-07/Food-Ordering-App-Backend",
+    },
+    {
+        id: "3",
+        label: "Social Media Manager",
+        image: SocialMediaApi,
+        repoLink: "https://github.com/vitthal-07/Social-Media-Manager",
+    },
+    {
+        id: "2",
+        label: "Amazone UI Clone",
+        image: Amazon,
+        repoLink: "https://github.com/vitthal-07/Amazon-Clone",
+        demoLink: "",
+    },
+    {
+        id: "2",
+        label: "Netflix UI Clone",
+        image: Netflix,
+        repoLink: "https://github.com/vitthal-07/Netflix-Clone",
+        demoLink: "",
+    },
+];
